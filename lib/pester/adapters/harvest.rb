18d7c7fb6281eed02894c8697220412b5c673314
Bob Nadler <bnadlerjr@gmail.com>
2014-02-23 11:41:13 -0500

    Handle Harvest errors.

--- lib/pester/adapters/harvest.rb
+++ lib/pester/adapters/harvest.rb
@@ -1,15 +1,26 @@
 module Pester
   module Adapters
     class Harvest
-      attr_reader :client
+      attr_reader :client, :log
 
-      def initialize(subdomain, username, password)
+      def initialize(subdomain, username, password, **opts)
         @client = ::Harvest.hardy_client(subdomain, username, password)
+        @log = opts.fetch(:log) { Pester::adapters[:log] }
       end
 
       def hours_for(email, as_of)
         user = client.users.find(email)
         hours = client.reports.time_by_user(user.id, as_of - 7, as_of).map(&:hours).reduce(0, &:+)
+      rescue ::Harvest::NotFound => error
+        log.error("Could not find employee in Harvest (#{email})")
+        -1
+      rescue ::Harvest::HTTPError => error
+        log.error("An HTTP error occurred while making a request to Harvest")
+        log.error("Hint: #{error.hint}") if error.hint
+        log.error("Params: #{error.params}")
+        log.error("Response: #{error.response}")
+        log.error(error.backtrace.join("\n"))
+        -1
       end
     end
   end
--- test/large/harvest_test.rb
+++ test/large/harvest_test.rb
@@ -1,13 +1,27 @@
 require_relative "../test_helper"
 require_relative "../../lib/pester/adapters/harvest"
 require "harvested"
+require "logger"
 
 module Pester
   module Adapters
     class HarvestTest < Test::Unit::TestCase
+      let(:log_stream) { StringIO.new }
+      let(:client) { Harvest.new(
+          ENV["HARVEST_SUBDOMAIN"],
+          ENV["HARVEST_USERNAME"],
+          ENV["HARVEST_PASSWORD"],
+          log: Logger.new(log_stream)
+      )}
+
       test "get hours for employee as of date" do
-        h = Harvest.new(ENV["HARVEST_SUBDOMAIN"], ENV["HARVEST_USERNAME"], ENV["HARVEST_PASSWORD"])
-        assert_equal(42.0, h.hours_for("Bnadler@cyrusinnovation.com", Date.new(2014, 2, 15)))
+        assert_equal(42.0, client.hours_for("Bnadler@cyrusinnovation.com", Date.new(2014, 2, 15)))
+      end
+
+      test "get hours for employee that cannot be found" do
+        assert_equal(-1, client.hours_for("no_such_email", Date.new(2014, 2, 15)))
+        log_stream.rewind
+        assert(/Could not find employee in Harvest \(no_such_email\)/ =~ log_stream.entries.join(""))
       end
     end
   end
